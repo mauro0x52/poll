813103f72ce2c0ef106889f6818e064c762a9983
Empreendemia <cadastros@empreendemia.com.br>
2012-07-25 19:02:59 +0000

    alteracoes

--- newQuestion.js
+++ newQuestion.js
@@ -1,11 +1,10 @@
 var model = require('./model.js');
 
 var question = new model.Question({
-    question : "Qual das op??es se encaixa melhor no seu dia a dia?",
+    question : "Voce tem interesse em uma ferramenta que simplifique a sua compra de anuncios na internet",
     answers  : [
-        {value : "Sempre consigo cumprir meu planejamento"},
-        {value : "No geral, sou produtivo, mas sinto que me falta tempo para algumas tarefas"},
-        {value : "Estou sempre correndo e me sobra pouco tempo para planejar"}
+        {value : "Sim"},
+        {value : "Nao"}
     ]
 });
 
