5970cc16a051acd56eeec928f78eec89f630b12a
Rafael Almeida Erthal Hermano <rafaerthal@gmail.com>
2012-08-07 15:29:58 -0300

    Update newQuestion.js

--- newQuestion.js
+++ newQuestion.js
@@ -1,14 +1,26 @@
 var model = require('./model.js');
 
 var question = new model.Question({
-    question : "Voce tem interesse em uma ferramenta que simplifique a sua compra de anuncios na internet",
+    question : "O que você acha do Empreendemia ser integrado com ferramentas de gestão?",
     answers  : [
-        {value : "Sim"},
-        {value : "Nao"}
+        {value : "Muito bom"},
+        {value : "Bom"},
+        {value : "Indiferente"},
+        {value : "Ruim"}
     ]
 });
 
-question.save(function(err){
-    if(err) console.log(err)
-    else console.log("Pergunta cadastrada com sucesso!");
+model.Question.find(function(err, questions) {
+    var i;
+
+    if (err) console.log(err);
+    
+    for (i = 0; i < questions.length; i ++) {
+        questions[i].remove();
+    }
+    
+    question.save(function(err){
+        if(err) console.log(err)
+        else console.log("Pergunta cadastrada com sucesso!");
+    });
 });
